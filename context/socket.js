46b5d158e80366b7aa637845ddc4fef46a0a93c7
Ajay Sinha <ajaysinhaa09@gmail.com>
2024-11-23 22:49:16 +0530

    formated code

--- context/socket.js
+++ context/socket.js
@@ -12,16 +12,16 @@ export const SocketProvider = ({ children }) => {
   const [socket, setSocket] = useState(null)
 
   useEffect(() => {
-      const connection = io()
-      console.log("connection", connection)
+    const connection = io()
+    console.log("connection", connection)
     setSocket(connection)
     return () => connection.close()
   }, [])
-    
-    socket?.on("connect_error", async (error) => {
-        console.log('connection error', error)
-        await fetch('/api/socket')
-    })
+
+  socket?.on("connect_error", async (error) => {
+    console.log("connection error", error)
+    await fetch("/api/socket")
+  })
 
   return (
     <SocketContext.Provider value={socket}>{children}</SocketContext.Provider>
